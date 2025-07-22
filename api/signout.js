a982b847a093392a5f85155def73f11b900e75d2
KnTanveer <kntanveer7@gmail.com>
2025-07-22 12:19:57 +0530

    Delete api directory

--- api/signout.js
+++
@@ -1,9 +0,0 @@
-import cookie from 'cookie';
- 
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', cookie.serialize('token', '', {
-    path: '/',
-    expires: new Date(0),
-  }));
-  res.redirect('/');
-}
