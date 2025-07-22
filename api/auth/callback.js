da8098709e0a7e79bc21afaa0849ad9b8a896f2a
KnTanveer <kntanveer7@gmail.com>
2025-07-22 16:44:18 +0530

    Update callback.js

--- api/auth/callback.js
+++ api/auth/callback.js
@@ -14,9 +14,9 @@ export default async function handler(req, res) {
   });
 
   res.setHeader("Set-Cookie", [
-    `token=${data.access_token}; Path=/; HttpOnly; Max-Age=3600; SameSite=Lax`,
-    `refresh=${data.refresh_token}; Path=/; HttpOnly; Max-Age=2592000; SameSite=Lax`
+    `access_token=${data.access_token}; Path=/; HttpOnly; Max-Age=3600; SameSite=Lax`,
+    `refresh_token=${data.refresh_token}; Path=/; HttpOnly; Max-Age=2592000; SameSite=Lax`
   ]);
-
+  
   res.redirect('/');
 }
