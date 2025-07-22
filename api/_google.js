70b03736d100e691d493aeb9013c2872bd437dda
KnTanveer <kntanveer7@gmail.com>
2025-07-22 22:03:00 +0530

    Update _google.js

--- api/_google.js
+++ api/_google.js
@@ -2,7 +2,18 @@ import { google } from "googleapis";
 
 export function getSessionClient(tokens) {
   const { client_id, client_secret, redirect_uris } = getGoogleConfig();
-  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  if (!client_id || !client_secret || !redirect_uris[0]) {
+    console.error("Missing Google OAuth environment variables");
+    throw new Error("Google OAuth config is incomplete");
+  }
+
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
   oAuth2Client.setCredentials(tokens);
   return oAuth2Client;
 }
@@ -11,6 +22,6 @@ function getGoogleConfig() {
   return {
     client_id: process.env.GOOGLE_CLIENT_ID,
     client_secret: process.env.GOOGLE_CLIENT_SECRET,
-    redirect_uris: [process.env.REDIRECT_URI]
+    redirect_uris: [process.env.REDIRECT_URI],
   };
 }
